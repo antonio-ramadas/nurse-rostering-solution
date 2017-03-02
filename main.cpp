d57721bc9175146388046e4ec8682d1a8b46cfc6
António Ramadas <antonio@wifi-interno.fe.up.pt>
2017-03-02 15:31:46 +0000

    C++14

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by António Ramadas on 02/03/2017.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Parse
+
+    //Compute
+
+    //Dump result
+
+    return EXIT_SUCCESS;
+}
