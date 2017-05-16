25bb9b32aa4e440ac7eeee2dbf4cd3bd619b9b29
F0lha <pedrovieiracastro@hotmail.com>
2017-05-16 20:19:29 +0100

    Created Validator

---
+++ code/Validator.h
@@ -0,0 +1,24 @@
+//
+// Created by F0lha on 16/05/2017.
+//
+
+#ifndef NRS_VALIDATOR_H
+#define NRS_VALIDATOR_H
+
+
+#include "solution/Solution.h"
+
+using namespace std;
+
+static class Validator {
+
+public:
+
+public:
+    bool contraintH1(Solution solution);
+    bool contraintH1(NurseSolution nurse);
+    bool contraintH2(Solution solution);
+};
+
+
+#endif //NRS_VALIDATOR_H
